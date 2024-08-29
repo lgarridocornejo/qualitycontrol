a572138ced5e754a054fc29297a25ba51432528b
Luis Garrido <lgarrido.cornejo@gmail.com>
2024-08-29 16:49:38 -0400

    matplotlib

--- pages/01_duplicados.py
+++ pages/01_duplicados.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import matplotlib.pyplot as plt
+#import plotly.express as px
 
 st.title("Análisis de Duplicados")
 
--- pages/02_estandares.py
+++ pages/02_estandares.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 
 
 st.title("Página en construcción")
