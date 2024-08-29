28fd788226515e51886ee407bb1adf126a8d4bff
Luis Garrido <lgarrido.cornejo@gmail.com>
2024-08-29 11:57:06 -0400

    20240829 inicio

---
+++ pages/01_duplicados.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+from plotly import express as px
+
+st.title("Análisis de Duplicados")
+
+
+#st.write("Subir archivo")
+file_upload = st.file_uploader("Subir archivo para analisis",type=['csv'])
+
+if file_upload:
+    df = pd.read_csv(file_upload)
+    st.write(df.head())
+
+    colorg = df.columns
+    featorg = st.selectbox('Seleccionar columna original',options=colorg)
+    featdup = st.selectbox('Seleccionar columna duplicado',options=colorg)
+
+    st.write(featorg)
+    st.write(featdup)
+
+     
+    
